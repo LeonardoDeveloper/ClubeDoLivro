e0f6f1e143badb8d75ecd499774fe350f8029508
Leonardo Said <leonardo.carvalho.said@outlook.com>
2024-11-17 20:46:34 -0300

    Commit inicial do projeto em que foram desenvolvidas as classes de autor, emprestimo, livro e usuario

---
+++ ClubeDoLivro/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace ClubeDoLivro
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+    }
+}
